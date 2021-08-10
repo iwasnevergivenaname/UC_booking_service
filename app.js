cfeab98394b3ab1875339f6530bf732082dfef0e
iwasnevergivenaname <hannah.mace93@gmail.com>
2021-08-10 16:00:36 -0700

    communication with ride service working

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+require('dotenv').config()
 const express = require("express")
+const cors = require("cors")
+
 const app = express()
+
 const routingRoutes = require('./routes/routing')
 
+app.use(cors())
 app.use(express.json())
 app.use('/routing', routingRoutes)
 
--- routes/routing.js
+++ routes/routing.js
@@ -1,15 +1,28 @@
+require('dotenv').config({ path: '../.env' })
 const express = require("express")
-const uuid = require("uuid")
+const { v4: uuidv4 } = require('uuid')
+const axios = require("axios")
+
 const router = new express.Router()
 
-router.post("/", (req, res) => {
+const rideExperienceEndpoint = `${process.env.NODE_RIDE_EXPERIENCE_SERVICE_ENDPOINT}`
+
+
+router.post("/", async (req, res) => {
   const {pickup, dropoff} = req.body
+  const reqId = uuidv4()
   // TAKES PICKUP AND DROP OFF, CREATES A POSSIBLE ROUTE TO TAKE
 
   // SENT HTTP REQ TO RIDE EXPERIENCE SERVICE
     // TO ENQUEUE THIS TRIP
-
+  try {
+    const rideRes = await axios.post(rideExperienceEndpoint, {reqId: reqId})
   // RETURNS 200 AND REQUEST ID
+    return res.json({code: 200, reqId: reqId})
+  } catch (e) {
+    console.log(e)
+  }
+
 })
 
 router.get("/", (req, res) => {
